c8cc45a1aa79e6478644937b9a1516c79dd8e914
Prabhathiya Herath <134810337+rpherath@users.noreply.github.com>
2024-07-27 11:05:16 +0530

    Delete solve.m

--- solve.m
+++
@@ -1,8 +0,0 @@
-function [T,Y] = solve(Y,V,Delta,Nz)
-     global delT;
-     tspan = linspace(0,delT,10); % Defining the time span -> Start time,End time,number of steps
-     [T,Y] = ode45(@(t,x) BicycleModel(t,x,V,Delta,Nz),tspan,Y); %Solving the set of 1st order diff equations
- 
-     %[T,Y] = ode45(@(t,x) MotorSISO(t,x,u,J,Kt,L,Kb,R),tspan,x);
-end
-
